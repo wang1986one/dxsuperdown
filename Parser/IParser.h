99e482a063dde0e292f2ec16a7b8cd37b926b7d6
daviyang35@gmail.com <daviyang35@gmail.com@cd019be1-ac5a-8332-f707-89159515352f>
2013-06-14 02:03:57 +0000

    项目结构调整 一些小的代码结构改动

--- Parser/IParser.h
+++ Parser/IParser.h
@@ -1,20 +1,17 @@
 #pragma once
-#include <map>
-
-typedef std::map<CString,CString> VString;
 
 class ParserRequest
 {
 	CString ID;					// 需要解析的ID
 	CString UserName;		// 用户名
 	CString Password;			// 密码
-	VString Information;		// 其他数据
+	MString Information;		// 其他数据
 };
 
 class ParserResponse
 {
 	CString RealAddress;		// 真实地址
-	VString Information;		// 其他数据
+	MString Information;		// 其他数据
 };
 
 class IParser
